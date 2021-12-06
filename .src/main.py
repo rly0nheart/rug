96d8b8a75cee58c84c5666c4cd49908c9b2b96fb
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-12-06 22:39:53 +0200

    Update main.py

--- .src/main.py
+++ .src/main.py
@@ -46,7 +46,7 @@ start = datetime.now()
 parser = argparse.ArgumentParser(description=f"{green}Rug{white}: A free, open-source tool for generating random user data. Like {green}Lorem Ipsum{white}, but for people. Developed by {green}Richard Mwewa{white} | https://github.com/{green}rlyonheart{reset}")
 parser.add_argument("-r","--results",help=f"{white}results count ({green}default is 10{white}){reset}", metavar=f"{white}[NUMBER]{reset}", dest="results", default=10)
 parser.add_argument("-n", "--nationality", help=f"{white}specify nationalit(y)(ies). If more than one, they ahould be seperated by a coma({green}es{white}, {green}gb{white}, {green}af{white}, {green}us{white}, {green}eu{white}). Default takes all available nationalities{reset}", metavar=f"{white}[NATIONALIT(Y)(IES)]{reset}", dest="nationality", default="es,gb,af,us,eu")
-parser.add_argument("-v", "--verbose", help=f"{white}run Rug in {green}verbose{white} mode({green}show network logs,errors, and notices{white}){reset}", dest="verbose", action="store_true")
+parser.add_argument("-v", "--verbose", help=f"{white}[RECOMMENDED] enable verbosity ({green}show network logs,errors, and notices{white}){reset}", dest="verbose", action="store_true")
 args = parser.parse_args()
 if args.verbose:
 		logging.basicConfig(format=f"{white}[{green}*{white}] %(message)s{reset}",level=logging.DEBUG)
